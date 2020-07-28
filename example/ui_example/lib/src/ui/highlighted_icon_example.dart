f82a3d440ab066d0361ce81321f64ea5d3301f3d
BugTheDebugger <bprayush@yahoo.com>
2020-07-28 18:45:07 +0545

    Updating ui examples

--- example/ui_example/lib/src/ui/highlighted_icon_example.dart
+++ example/ui_example/lib/src/ui/highlighted_icon_example.dart
@@ -6,7 +6,7 @@ class HighlightedIconExample extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('School location widget'),
+        title: Text('Highlighted Icon'),
         centerTitle: true,
       ),
       body: Center(
--- example/ui_example/lib/src/ui/notice_card_example.dart
+++ example/ui_example/lib/src/ui/notice_card_example.dart
@@ -14,7 +14,7 @@ class NoticeCardExample extends StatelessWidget {
         padding: const EdgeInsets.all(8.0),
         child: Center(
           child: NoticeCard(
-            date: DateFormat('yyyy-MM-DD').format(DateTime.now()),
+            date: DateFormat('yyyy-MM-dd').format(DateTime.now()),
             title: 'School Reopens',
             subTitile:
                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
